641789914d383f2afc7dd3c15cdff766e2bd029f
Mariam1234216 <qali97465@gmail.com>
2026-06-28 01:45:36 +0500

    Create webhook.js

---
+++ api/webhook.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    console.log('Webhook received:', req.body);
+    return res.status(200).json({ message: 'Webhook received successfully' });
+  } else {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+}
